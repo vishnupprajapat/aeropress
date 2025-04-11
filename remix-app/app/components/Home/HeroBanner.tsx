37fee5d0d16f262afa7247754b5e71ffcbeb4ce2
Vishnu Prajapat <dev.vishnuprajapat@gmail.com>
2025-04-11 18:37:36 +0530

    Refactor HeroBanner component: clean up image loading attributes and improve code readability

--- remix-app/app/components/Home/HeroBanner.tsx
+++ remix-app/app/components/Home/HeroBanner.tsx
@@ -1,54 +1,48 @@
 import { Link } from "@remix-run/react"
 import { stegaClean } from "@sanity/client/stega"
-const HeroBanner = ({HeroBannerData}:any) => {
+const HeroBanner = ({ HeroBannerData }: any) => {
     const imageUrl = HeroBannerData?.data.herosection?.backgroundImage?.asset?.url || "https://cdn.sanity.io/images/n8l6ew8i/production/a08327a03de3344ce432d9ed401d6ba9606bafd6-1920x818.webp"
     const imageWidth = HeroBannerData?.data.herosection?.backgroundImage?.asset?.metadata?.dimensions?.width
     const imageHeight = HeroBannerData?.data.herosection?.backgroundImage?.asset?.metadata?.dimensions?.height
     const imageUrlForMobile = HeroBannerData?.data.herosection?.backgroundImageForMobile?.asset?.url
-    const imageWidthForMobile = HeroBannerData?.data.herosection?.backgroundImageForMobile?.asset?.metadata?.dimensions?.width
-    const imageHeightForMobile = HeroBannerData?.data.herosection?.backgroundImageForMobile?.asset?.metadata?.dimensions?.height
     const badge = HeroBannerData?.data.herosection?.badge
     const ctaText = HeroBannerData?.data.herosection?.ctaText
     const ctaUrl = HeroBannerData?.data.herosection?.ctaUrl
-    const heading =  HeroBannerData?.data.herosection?.heading
+    const heading = HeroBannerData?.data.herosection?.heading
     const features = HeroBannerData?.data.herosection?.features
 
     return (
         <div className="home-banner">
             <div className="home-banner-image">
-                <img 
-                    className="desktop-image" 
+                <img
+                    className="desktop-image"
                     src={imageUrl}
                     width={imageWidth}
                     height={imageHeight}
                     rel="preload"
-                    loading="eager"
-                    {...{ fetchpriority: 'high' } as any}
                     alt={stegaClean(heading)}
                 />
-                <img 
+                <img
                     className="mobile-image"
                     src={imageUrlForMobile}
                     alt={stegaClean(heading)}
                     loading="eager"
-                    {...{ fetchpriority: 'high' } as any}
-                    width={imageWidthForMobile}
-                    height={imageHeightForMobile}
-                 />
+                    fetchPriority="high"
+                />
             </div>
             <div className="home-banner-content">
                 <div className="home-banner-content-warper">
-                <div className="announcement-bar__message text--xsmall">
-                    <p>
-                    <span className="message--stars">★ ★ ★ ★ ★</span> 
-                    {stegaClean(badge)}</p>
+                    <div className="announcement-bar__message text--xsmall">
+                        <p>
+                            <span className="message--stars">★ ★ ★ ★ ★</span>
+                            {stegaClean(badge)}</p>
                     </div>
                     <h1 className="banner-heading">{stegaClean(heading)}</h1>
                     <div className="home-banner-list">
                         <ul>
                             {
-                                features?.map((item:any,key:number)=>{
-                                    return(
+                                features?.map((item: any, key: number) => {
+                                    return (
                                         <li key={key}><span>{item}</span></li>
                                     )
                                 })
