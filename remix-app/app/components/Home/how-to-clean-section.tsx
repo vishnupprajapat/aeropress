4a58061e5a3b399be2a661da3a7bccb0143c93b6
Vishnu Prajapat <dev.vishnuprajapat@gmail.com>
2025-04-14 15:40:17 +0530

    Enhance HowToCleanSection component: implement responsive SlickSlider for steps; add autoplay feature; update CSS for improved layout and spacing

--- remix-app/app/components/Home/how-to-clean-section.tsx
+++ remix-app/app/components/Home/how-to-clean-section.tsx
@@ -1,9 +1,47 @@
 import { stegaClean } from '@sanity/client/stega'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { urlFor } from '~/sanity/image'
 import Image from './Image'
+let SlickSlider: any;
+
+if (typeof window !== "undefined") {
+  SlickSlider = require("react-slick").default;
+}
 
 const HowToCleanSection = ({steps,stepsTitle}:any) => {
+         const [autoplay, setAutoplay] = useState(false);
+        
+          useEffect(() => {
+            const timer = setTimeout(() => setAutoplay(true), 1000); 
+            return () => clearTimeout(timer);
+          }, []);
+          const settings = {
+            slidesToShow: 4,
+            responsive: [
+              { breakpoint: 998, settings: { 
+                dots: true,
+                infinite: true,
+                speed: 500,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                autoplay: autoplay,
+                autoplaySpeed: 4000,
+               } },
+              {
+                breakpoint: 640,
+                settings: {
+                  slidesToShow: 1,
+                  dots: true,
+                },
+              },
+            ],
+          };
+        
+          if (typeof window === "undefined" || steps.length === 0) {
+            return null;
+          }
+        
+        
   return (
     <section className='section section--flush home-multi-3column how_to_clean_column'>
         <div className="container">
@@ -11,36 +49,39 @@ const HowToCleanSection = ({steps,stepsTitle}:any) => {
                 <h2 className="heading h2">{stegaClean(stepsTitle)}</h2>
             </div>
             <div className="multi-column multi-blocks-size4 multi-column--pocket-medium multi-column--medium multi-column--spacing-normal">
-                <div className="multi-column__inner multi-column__inner--center ">
-                    {steps.map((step:any, index:number) => {
-                        const {title, description, image} = step
-                        const imageUrl = image?.asset?.url || "https://aeropress.com/cdn/shop/files/Pour_59ead110-03e0-4fcf-8921-924101cb46be_500x.png?v=1713529053"
-                        const imageWidth = image?.asset?.metadata?.dimensions?.width || 500
-                        const imageHeight = image?.asset?.metadata?.dimensions?.height || 500
-                        const imageLqip = image?.asset?.metadata?.lqip || "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
-                        const imageSrcSet = `${imageUrl} ${imageWidth}w, ${imageLqip} 1x`
-                        return(
-                            <div key={index} className="multi-column__item small ">
-                            <div className="multi-column__image-wrapper">
-                            <Image
-                                className="multi-column__image"
-                                src={urlFor(image).url()}
-                                srcSet={imageSrcSet }
-                                width={imageWidth}
-                                height={imageHeight}
-                                alt={stegaClean(title)}
-                                loading="lazy"
-                            />
+            {SlickSlider && (
+                <SlickSlider {...settings}  className="multi-column__inner multi-column__inner--center ">
+                {steps.map((step:any, index:number) => {
+                    const {title, description, image} = step
+                    const imageUrl = image?.asset?.url || "https://aeropress.com/cdn/shop/files/Pour_59ead110-03e0-4fcf-8921-924101cb46be_500x.png?v=1713529053"
+                    const imageWidth = image?.asset?.metadata?.dimensions?.width || 500
+                    const imageHeight = image?.asset?.metadata?.dimensions?.height || 500
+                    const imageLqip = image?.asset?.metadata?.lqip || "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
+                    const imageSrcSet = `${imageUrl} ${imageWidth}w, ${imageLqip} 1x`
+                    return(
+                        <div key={index} className="multi-column__item small ">
+                        <div className="multi-column__image-wrapper">
+                        <Image
+                            className="multi-column__image"
+                            src={urlFor(image).url()}
+                            srcSet={imageSrcSet }
+                            width={imageWidth}
+                            height={imageHeight}
+                            alt={stegaClean(title)}
+                            loading="lazy"
+                        />
 
-                            </div>
-                            <div className="multi-column__text-container text--center text-container">
-                                <p className="heading h5">{stegaClean(title)}</p>
-                                <p><strong>{description}</strong></p>
-                            </div>
                         </div>
-                        )
-                        })}
-                </div>
+                        <div className="multi-column__text-container text--center text-container">
+                            <p className="heading h5">{stegaClean(title)}</p>
+                            <p><strong>{description}</strong></p>
+                        </div>
+                    </div>
+                    )
+                    })}
+            </SlickSlider>
+            )}
+
             </div>
         </div>
     </section>
--- remix-app/app/components/Home/product-section.tsx
+++ remix-app/app/components/Home/product-section.tsx
@@ -6,7 +6,7 @@ import { stegaClean } from '@sanity/client/stega'
 let SlickSlider: any;
 
 if (typeof window !== "undefined") {
-  SlickSlider = require("react-slick").default;
+    SlickSlider = require("react-slick").default;
 }
 
 
@@ -15,14 +15,14 @@ const ProductSection = ({ productData }: any) => {
     const compareText = productData?.data?.productSection?.compareText
     const product = productData?.data?.productSection?.products
 
-     const [autoplay, setAutoplay] = useState(false);
-    
-      useEffect(() => {
+    const [autoplay, setAutoplay] = useState(false);
+
+    useEffect(() => {
         const timer = setTimeout(() => setAutoplay(true), 1000); // Delay autoplay to reduce layout shift
         return () => clearTimeout(timer);
-      }, []);
-    
-      const settings = {
+    }, []);
+
+    const settings = {
         dots: true,
         infinite: true,
         speed: 500,
@@ -31,31 +31,31 @@ const ProductSection = ({ productData }: any) => {
         autoplay: autoplay,
         autoplaySpeed: 4000,
         responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 2,
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                },
             },
-          },
-          {
-            breakpoint: 640,
-            settings: {
-              slidesToShow: 2,
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 2,
+                },
             },
-          },
-          {
-            breakpoint: 445,
-            settings: {
-              slidesToShow: 1,
+            {
+                breakpoint: 445,
+                settings: {
+                    slidesToShow: 1,
 
+                },
             },
-          },
         ],
-      };
-    
-      if (typeof window === "undefined" || productData.length === 0) {
+    };
+
+    if (typeof window === "undefined" || productData.length === 0) {
         return null;
-      }
+    }
     return (
         <section className='section home-featured-collections'>
             <div className="section-wrapper">
@@ -65,77 +65,82 @@ const ProductSection = ({ productData }: any) => {
                     </div>
                     <div className="featured-collections">
                         {SlickSlider && (
-                            <SlickSlider {...settings}  className="product-list__inner ">
-                            {product?.map((item: any, key: number) => {
-                                const imageUrl = item?.image?.asset?.url
+                            <SlickSlider {...settings} className="product-list__inner ">
+                                {product?.map((item: any, key: number) => {
+                                    const imageUrl = item?.image?.asset?.url
                                     const imageWidth = item?.image?.asset?.metadata?.dimensions?.width
                                     const imageHeight = item?.image?.asset?.metadata?.dimensions?.height
                                     const imageLqip = item?.image?.asset?.metadata?.lqip
                                     const imageSrcSet = `${imageUrl} ${imageWidth}w, ${imageLqip} 1x`
-                                const hoverImageUrl = item?.hoverImage?.asset?.url
+                                    const hoverImageUrl = item?.hoverImage?.asset?.url
                                     const hoverImageWidth = item?.hoverImage?.asset?.metadata?.dimensions?.width
                                     const hoverImageHeight = item?.hoverImage?.asset?.metadata?.dimensions?.height
                                     const hoverImageLqip = item?.hoverImage?.asset?.metadata?.lqip
                                     const hoverImageSrcSet = `${hoverImageUrl} ${hoverImageWidth}w, ${hoverImageLqip} 1x`
-                                const title = item?.title
-                                const subtitle = item?.subtitle
-                                const price = item?.price
-                                const ctaText = item?.ctaText
-                                const variantImages = item?.variantImages
-                                return (
-                                    <div key={key} className="new-product-item">
-                                        <div className="product-item ">
-                                            <div className="product-item__image-wrapper product-item__image-wrapper--multiple">
-                                                <Image
-                                                className='product-item__primary-image ' 
-                                                src={urlFor(imageUrl).url()}
-                                                    srcSet={imageSrcSet}
-                                                    width={imageWidth}
-                                                    height={imageHeight}
-                                                    alt={title}
-                                                    loading="lazy"
-                                                />
-                                                <img width={hoverImageWidth} height={hoverImageHeight} loading="lazy" className='product-item__secondary-image' src={hoverImageUrl} />
-                                            </div>
-                                            <div className="product-item__info product-item__info--with-button ">
-                                                <div className="product-item-meta">
-                                                    <Link to="/" className="product-item-meta__title">{stegaClean(title)}</Link>
-                                                    <span className="hidden-phone">{subtitle}</span>
-                                                    <div className="product-item-meta__price-list-container">
-                                                        <span className='price'>{price}</span>
+                                    const title = item?.title
+                                    const subtitle = item?.subtitle
+                                    const price = item?.price
+                                    const ctaText = item?.ctaText
+                                    const variantImages = item?.variantImages
+                                    return (
+                                        <div key={key} className="new-product-item">
+                                            <div className="product-item ">
+                                                <div className="product-item__image-wrapper product-item__image-wrapper--multiple">
+                                                    {(key === 1 || key === 4) && (
+                                                        <div className="product-item__label-list label-list">
+                                                            <span className="label label--custom new">new</span>
+                                                        </div>
+                                                    )}
+                                                    <Image
+                                                        className='product-item__primary-image '
+                                                        src={urlFor(imageUrl).url()}
+                                                        srcSet={imageSrcSet}
+                                                        width={imageWidth}
+                                                        height={imageHeight}
+                                                        alt={title}
+                                                        loading="lazy"
+                                                    />
+                                                    <img width={hoverImageWidth} height={hoverImageHeight} loading="lazy" className='product-item__secondary-image' src={hoverImageUrl} />
+                                                </div>
+                                                <div className="product-item__info product-item__info--with-button ">
+                                                    <div className="product-item-meta">
+                                                        <Link to="/" className="product-item-meta__title">{stegaClean(title)}</Link>
+                                                        <span className="hidden-phone">{subtitle}</span>
+                                                        <div className="product-item-meta__price-list-container">
+                                                            <span className='price'>{price}</span>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div className="product-details__size">
-                                                    {
-                                                        variantImages?.map((item: any, key: number) => {
-                                                            const imgurl = item?.asset?.url;
-                                                            return (
-                                                                <div key={key} className="product_option color">
-                                                                    <div className="color">
-                                                                        <span
-                                                                            className="product_option-color-image"
-                                                                            style={{
-                                                                                backgroundImage: `url(${imgurl})`,
-                                                                                backgroundSize: 'cover',
-                                                                                backgroundPosition: 'center',
-                                                                            }}
-                                                                        ></span>
+                                                    <div className="product-details__size">
+                                                        {
+                                                            variantImages?.map((item: any, key: number) => {
+                                                                const imgurl = item?.asset?.url;
+                                                                return (
+                                                                    <div key={key} className="product_option color">
+                                                                        <div className="color">
+                                                                            <span
+                                                                                className="product_option-color-image"
+                                                                                style={{
+                                                                                    backgroundImage: `url(${imgurl})`,
+                                                                                    backgroundSize: 'cover',
+                                                                                    backgroundPosition: 'center',
+                                                                                }}
+                                                                            ></span>
+                                                                        </div>
                                                                     </div>
-                                                                </div>
-                                                            );
-                                                        })
-                                                    }
+                                                                );
+                                                            })
+                                                        }
 
-                                                </div>
-                                                <div className="add-to-cart-btn">
-                                                    <button className=''>{ctaText}</button>
+                                                    </div>
+                                                    <div className="add-to-cart-btn">
+                                                        <button className=''>{ctaText}</button>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                )
-                            })}
-                        </SlickSlider>
+                                    )
+                                })}
+                            </SlickSlider>
                         )}
                     </div>
                     <div className="collection_maker_btn">{compareText}</div>
